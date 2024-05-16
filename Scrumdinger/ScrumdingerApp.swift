6709a20b58f7914ab2acd268eeb478ef28cbdb55
Aryan Patel <aryanpatel31@yahoo.com>
2024-05-15 22:55:57 -0500

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Aryan Patel on 5/15/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
